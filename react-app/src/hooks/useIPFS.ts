6d4f7378e20b898fd18dbc3fefdb3eec75b4c476
devarogundade <devarogundade@gmail.com>
2025-10-25 00:01:34 +0100

    update

--- react-app/src/hooks/useIPFS.ts
+++ react-app/src/hooks/useIPFS.ts
@@ -2,7 +2,7 @@ import { PinataSDK } from "pinata";
 
 const pinata = new PinataSDK({
   pinataJwt: import.meta.env.VITE_PINATA_JWT,
-  pinataGateway: "example-gateway.mypinata.cloud",
+  pinataGateway: import.meta.env.VITE_PINATA_GATEWAY,
 });
 
 export function useIPFS() {
